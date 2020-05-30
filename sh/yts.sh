c41d1f35198f241025a5cccf9de973709b5e4c81
Madison Gaydragon Brooke <48199835+maddoxdragon@users.noreply.github.com>
2020-05-30 03:21:12 -0500

    FIX CYTS: I USED A RELATIVE FILEPATH hgsagsghdsfv

--- sh/yts.sh
+++ sh/yts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 args="$@"
 echo "This is a placeholder so that the youtube results have the proper numbers" > ~/.centipeetle/txt/ytsout.txt
-API=`cat ../txt/ytapi` 
+API=`cat $HOME/.centipeetle/txt/ytapi` 
 curl 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q='"$args"'&type=video&key='"$API" | jq -r '.items[].id.videoId, .items[].snippet.title' >> ~/.centipeetle/txt/ytsout.txt
