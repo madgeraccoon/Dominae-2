1667f25a62fbfc6db9b068d7aa72c103abc3665c
Madison Gaydragon Brooke <48199835+maddoxdragon@users.noreply.github.com>
2020-05-30 03:20:52 -0500

    FIX CYT: I USED A RELATIVE FILEPATH hgsagsghdsfv

--- sh/yt.sh
+++ sh/yt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 args="$@"
-API=`cat ../txt/ytapi` 
+API=`cat $HOME/.centipeetle/txt/ytapi` 
 curl 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q='"$args"'&type=video&key='"$API" | jq -r '.items[].id.videoId, .items[].snippet.title' > ~/.centipeetle/txt/ytout.txt
